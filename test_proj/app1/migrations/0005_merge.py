badb21a0f584559ecb927af616bf3da843dcab1a
Dharmit Shah <shahdharmit@gmail.com>
2015-05-06 14:06:50 +0530

    Merge migrations

---
+++ test_proj/app1/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app1', '0004_auto_20150506_0834'),
+        ('app1', '0004_auto_20150506_0833'),
+    ]
+
+    operations = [
+    ]
