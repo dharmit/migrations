5e652546f798fdc500c075e1f311ad10fb94688d
Dharmit Shah <shahdharmit@gmail.com>
2015-05-06 14:05:18 +0530

    Dev branch - Merged migrations. Modified last name length from 255 to 250

---
+++ test_proj/app1/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app1', '0002_user_last_name'),
+        ('app1', '0002_auto_20150506_0810'),
+    ]
+
+    operations = [
+    ]
--- test_proj/app1/models.py
+++ test_proj/app1/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
 
     first_name = models.TextField(max_length=255)
-    last_name = models.TextField(max_length=255)
+    last_name = models.TextField(max_length=250)
 
     class Meta:
         db_table = 'users'
